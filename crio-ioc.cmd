a6a3220570edc275129b7ff029f92a74ecd365a5
Dawood Awny Dawood Alnajjar <dawood.alnajjar@sol11-linux.ABTLUS.org.br>
2019-06-26 09:32:31 -0300

    Adding updates cmd file

--- crio-ioc.cmd
+++ crio-ioc.cmd
@@ -1,14 +1,17 @@
-#!/usr/local/epics/apps/crio-ioc/bin/linux-x86_64/CRIO
+#!/home/ABTLUS/SOL/crio-ioc/bin/linux-x86_64/CRIO
 
 epicsEnvSet("TOP","/usr/local/epics/apps/crio-ioc")
 epicsEnvSet("EPICS_BASE","/usr/local/epics-nfs/base/R3.15.6")
 epicsEnvSet("IOC","iocCRIO")
 epicsEnvSet("CONFIG","/usr/local/epics/apps/config/crio-ioc")
-
+epicsEnvSet("AUTOSAVE","/opt/autosave")
 cd ${TOP}
-## Register all support components
+
 dbLoadDatabase "dbd/CRIO.dbd"
 CRIO_registerRecordDeviceDriver pdbbase
+set_requestfile_path($(CONFIG))
+set_savefile_path($(AUTOSAVE))
+set_pass1_restoreFile("crioioc.sav", "")
 
 crioSupSetup("${CONFIG}/cfg.ini" , 1)
 
@@ -22,7 +25,8 @@ dbLoadTemplate "${CONFIG}/scaler.db.sub"
 dbLoadTemplate "${CONFIG}/waveform.db.sub"
 iocInit
 
+< "$(CONFIG)/init-pv.cmd"
+
+create_monitor_set("crioioc.req", 1, "")
+
 dbl
-
-
-
